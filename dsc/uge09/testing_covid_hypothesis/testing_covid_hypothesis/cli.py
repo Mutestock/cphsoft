358b931f5ecaf7688992cf7dfe0f2d45b7092561
Mutestock <doublenegative@protonmail.com>
2021-04-10 14:31:15 +0200

    bizarre bug found...

--- dsc/uge09/testing_covid_hypothesis/testing_covid_hypothesis/cli.py
+++ dsc/uge09/testing_covid_hypothesis/testing_covid_hypothesis/cli.py
@@ -18,30 +18,34 @@ def manager():
 @click.option("--mean-choice", "-cm", nargs=2, help="Same as mean, but lets you pick which two munipalities you want to inspect the mean values of(Assuming it's part of the data set")
 def analysis(show, print, excel, compare, compare_choice, mean, mean_choice):
     if compare:
-        if no_tags_error_message(show,print,excel):   
-            logic.compare_municipalities_confirmed_cases(show=show, print_file=print, excel=excel)
+        if no_tags_error_message(show, print, excel):
+            logic.compare_municipalities_confirmed_cases(
+                show=show, print_file=print, excel=excel)
     elif compare_choice:
-        if no_tags_error_message(show,print,excel):  
-            logic.compare_municipalities_confirmed_cases(compare_choice[0], compare_choice[1], show=show, print_file=print, excel=excel)
+        if no_tags_error_message(show, print, excel):
+            logic.compare_municipalities_confirmed_cases(
+                compare_choice[0], compare_choice[1], show=show, print_file=print, excel=excel)
     elif mean:
-        if no_tags_error_message(show,print,excel):
-            logic.mean()
-        
-        
+        if no_tags_error_message(show, print, excel):
+            logic.mean(show=show, print_file=print, excel=excel)
+    elif mean_choice:
+        if no_tags_error_message(show, print, excel):
+            logic.mean(mean_choice[0], mean_choice[1],
+                       show=show, print_file=print, excel=excel)
 
 
 @manager.command()
 @click.option("--table", "-t", is_flag=True, help="Just shows one of the tables in the terminal")
 def basic(table):
     logic.display_dataframe(REGIONAL_FILE_TREE.get("noegle_tal"))
-    
 
-# Most commands needs extra informamtion about what they're supposed to do with the specified data.
-# E.g. --compare would just run the function and process the data as needed.
-# You need to tell the program what to do with it, i.e. do you want to print it, show it, and/or convert it to excel.
-# You must the command with at least one extra tag, but you can supply all of them tags.
-# These extra tags being: -s, -p or -ex. Respectively.
+
 def no_tags_error_message(show, print, excel):
+    # Most commands needs extra informamtion about what they're supposed to do with the specified data.
+    # E.g. --compare would just run the function and process the data as needed.
+    # You need to tell the program what to do with it, i.e. do you want to print it, show it, and/or convert it to excel.
+    # You must the command with at least one extra tag, but you can supply all of them tags.
+    # These extra tags being: -s, -p or -ex. Respectively.
     if show == False and print == False and excel == False:
         print("Nothing will happen unless you supply the command with either a -p (print), -s (show) or -ex (excel) tag")
         return False
--- dsc/uge09/testing_covid_hypothesis/testing_covid_hypothesis/logic.py
+++ dsc/uge09/testing_covid_hypothesis/testing_covid_hypothesis/logic.py
@@ -13,9 +13,10 @@ import os
 def display_dataframe(file):
     df = pd.read_csv(file)
     print(df)
-    
-def get_dataframes(a,b):
-    
+
+
+def get_dataframes(a, b):
+
     # Read a file in pandas and make it a dataframe.
     # The files used in the csv files are separated by semicolons
     df = pd.read_csv(COMMUNAL_FILE_TREE.get(
@@ -29,55 +30,60 @@ def get_dataframes(a,b):
     a_code = ""
     b_code = ""
 
-    # Making sure there's a hit. Informing user if there aren't.
-    
+    # Making sure there's a hit. Informing user if there isn't.
+
     try:
         a_code = int(LAU_CODES.get(a))
     except:
-        print(f"{a_code} is not a recognized input value")
-        return
+        print(f"\n\n{a} is not a recognized input value\n\n")
+        raise Exception
     try:
         b_code = int(LAU_CODES.get(b))
     except:
-        print(f"{b_code} is not a recognized input value")
-        return
+        print(f"\n\n{b} is not a recognized input value\n\n")
+        raise Exception
 
     #df = df[(df["Kommune"] == a_code) | (df["Kommune"] == b_code)]
     df_a = df[df["Kommune"] == a_code]
     df_b = df[df["Kommune"] == b_code]
-    
-    return (df_a, df_b)
+
+    return df, df_a, df_b, a_code, b_code
+
 
 def mean(a="copenhagen", b="aarhus", excel=False, show=False, print_file=False):
-    df_a, df_b = (get_dataframes(a,b))
+    df, df_a, df_b, a_code, b_code = get_dataframes(a, b)
 
 
 # Simple comparison of copenhagen and aarhus.
 def compare_municipalities_confirmed_cases(a="copenhagen", b="aarhus", excel=False, show=False, print_file=False):
 
-    df_a, df_b = (get_dataframes(a,b))
-    
+    df, df_a, df_b, a_code, b_code = get_dataframes(a, b)
+
     fig, ax = plt.subplots()
     myLocator = mticker.MultipleLocator(30)
     ax.xaxis.set_major_locator(myLocator)
-    
+
     ax.plot(df_a["Dato"], df_a["Bekraeftede tilfaelde"], '-', label=a)
     ax.plot(df_b["Dato"], df_b["Bekraeftede tilfaelde"], '-', label=b)
     ax.set_title(f'Bekræftede tilfælde pr dag pr kommune - {a} vs {b}')
-    ax.legend([f'{a}',f'{b}'])
-    #ax.xaxis_date()
+    ax.legend([f'{a}', f'{b}'])
+    # ax.xaxis_date()
     fig.autofmt_xdate()
     plt.xlabel("Date")
     plt.ylabel("Confirmed cases per day")
     if print_file:
-        plt.savefig(CUSTOM_IMAGES_PATH+f'{a}_{b}_bekraeftede_tilfælde_pr_dag_pr_kommune.png')
-    if excel:    
+        plt.savefig(CUSTOM_IMAGES_PATH +
+                    f'{a}_{b}_bekraeftede_tilfælde_pr_dag_pr_kommune.png')
+    if excel:
         df_ab = df[(df["Kommune"] == a_code) | (df["Kommune"] == b_code)]
         if not os.path.exists(CUSTOM_EXCEL_PATH):
             os.makedirs(CUSTOM_EXCEL_PATH)
-        with pd.ExcelWriter(CUSTOM_EXCEL_PATH + f"{a}_{b} bekraeftede tilfælde pr dag pr kommune.xlsx") as writer:    
-            df_ab.to_excel(writer, sheet_name =f"{a}_{b} bekraeftede tilfælde pr dag pr kommune")
-            df_a.to_excel(writer, sheet_name =f"{a} bekræftede tilfælde pr dag pr kommune")
-            df_b.to_excel(writer, sheet_name =f"{b} bekræftede tilfælde pr dag pr kommune")
+        with pd.ExcelWriter(CUSTOM_EXCEL_PATH + f"{a}_{b} bekraeftede tilfælde pr dag pr kommune.xlsx") as writer:
+            df_ab.to_excel(
+                writer, sheet_name=f"{a}_{b} bekraeftede tilfælde pr dag pr kommune")
+            df_a.to_excel(
+                writer, sheet_name=f"{a} bekræftede tilfælde pr dag pr kommune")
+            df_b.to_excel(
+                writer, sheet_name=f"{b} bekræftede tilfælde pr dag pr kommune")
     if show:
         plt.show()
